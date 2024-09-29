eab85b88b7ad17b9a83376f5539b3dd5a9aac077
Sathwik <ps.bits5@gmail.com>
2024-09-29 14:44:56 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,21 @@
 import java.util.Objects;
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args)
+public class Main { // class Main
+    public static void main(String[] args) // main()
     {
         Scanner in = new Scanner(System.in);
-        String trash = "";
-        System.out.print("Enter your affiliated party (D, R, I): ");
-        String partyAffiliation = in.next();
-        if(partyAffiliation.equals("D"))
-        {
+        String trash = "";                                           // String partyAffiliation
+        System.out.print("Enter your affiliated party (D, R, I): "); // output "Enter your affiliated party (D,R,I): "
+        String partyAffiliation = in.next();                         // input partyAffiliation
+        if(partyAffiliation.equals("D"))                             // if partyAffiliation == "D"
+                                                                     //     output ("You get a democratic donkey"
+        {                                                            // else if partyAffiliation == "R"
+                                                                        //     output ("You get a Republican elephant"
+                                                                     // else if partyAffiliation == "I"
+                                                                    //     output ("You get a democratic donkey"
+                                                                    // else
+                                                                   //     output ("You get other")
+
             System.out.println("You get a Democratic Donkey");
         }
         else if(partyAffiliation.equals("R"))
@@ -23,5 +30,5 @@ public class Main {
         {
             System.out.println("You get other");
         }
-    }
-}
+    } // return()
+} //end class
